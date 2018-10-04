19d9787be32fe81225e03cd3abeaf4e23318225b
Tim Michalski <tim@lssinc.com>
2018-10-03 22:35:20 -0500

    Fixed issues with loading files with latin-1 encoding

--- app/file_io.py
+++ app/file_io.py
@@ -11,7 +11,7 @@ import datetime
 def get_csv(csv_file_name):
     _clean_txt_file(csv_file_name)
 
-    with open(csv_file_name, 'r') as csv_file:
+    with open(csv_file_name, 'r', encoding='utf-8') as csv_file:
         reader = csv.reader(csv_file)
         csv_rows = list(reader)
 
@@ -26,7 +26,7 @@ def write_new_csv(original_file_name, dictionary_list):
     print('Writing results to ' + new_file_name)
 
     column_headers = _get_dictionary_keys(dictionary_list)
-    with open(new_file_name, mode='w') as csv_file:
+    with open(new_file_name, mode='w', encoding='utf-8') as csv_file:
         writer = csv.DictWriter(csv_file, fieldnames=column_headers)
         writer.writeheader()
         writer.writerows(dictionary_list)
@@ -37,12 +37,12 @@ def clean_txt(raw_txt):
 
 
 def _clean_txt_file(filename):
-    with open(filename, 'r') as txt_file:
+    with open(filename, 'r', encoding='latin-1') as txt_file:
         raw_txt = txt_file.read()
 
     cleaned_txt = clean_txt(raw_txt)
 
-    with open(filename, 'w') as txt_file:
+    with open(filename, 'w', encoding='utf-8') as txt_file:
         txt_file.write(cleaned_txt)
 
 
